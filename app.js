37cf9b6ecc9e715c4984069e526211d1e57d45e7
Jonathan Irving <j@lollyshouse.net>
2012-09-08 01:52:32 -0400

    Add HTTP basic shroud for testing.

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , auth = require('http-auth');
 
 var app = express();
+
+app.configure('development', function () {
+  app.use(express.logger({ format: ':method :url :status :remote-addr :response-time'}));
+});
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -29,8 +34,19 @@ app.configure('development', function(){
   app.locals.pretty = true;
 });
 
+// require HTTP basic
+var basic = auth({
+  authRealm: "Beach Energy - Private Area",
+  authList: ['beaches:beaches']
+});
+
+app.all('*', function (req, res, next) {
+  basic.apply(req, res, function () { next(); });
+});
+
+// Main routes
 app.get('/', routes.index);
-app.get('/users', user.list);
+// app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,6 @@ $(function () {
   $('.sign-in').live('click', function (event) {
     event.preventDefault();
 
-    $('.hero-unit h1').html('Wibble');
     $('#modal').modal('show');
   });
 });
