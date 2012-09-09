4e1cc3801b85f7cea04c2caaae2ca1f9b750ca22
Jonathan Irving <j@lollyshouse.net>
2012-09-09 00:09:05 -0400

    Make slug self-generate, close button work.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,32 +7,25 @@ $(function () {
 
   $('.new-article').live('click', function (event) {
     event.preventDefault();
-    $modal = $('#modal');
+    var $modal = $('#modal');
+
     $.ajax({
       url: '/new',
       dataType: 'html'
     }).done(function (html) {
-      $('.btn-primary').live('click', function (event) {
-        event.preventDefault();
-        $form = $modal.find('form');
-        $form.trigger('submit');
-
-        // $.post({
-        //   url: '/',
-        //   data: $form.serialize()
-        // }).done(function (html) {
-        //   $modal.modal('hide');
-        // });
-      });
-
-      $('.btn :not(.btn-primary)').live('click', function (event) {
-        event.preventDefault();
-        alert('hello');
-        $modal.modal('hide');
-      });
-
       $modal.find('.modal-header h3').html('New Article');
       $modal.find('.modal-body').html(html);
+
+      var $form = $modal.find('form');
+
+      $('.btn-primary').live('click', function (event) {
+        event.preventDefault();
+        $form.trigger('submit');
+      });
+
+      $form.find('input[name="title"]').live('focusout', function (event) {
+        $form.find('input[name="slug"]').attr('value', event.target.value.toLowerCase().replace(/ +/g, '-'));
+      });
 
       $modal.modal('show');
     });
